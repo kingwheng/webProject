58c39f01c85b4201b10762fa255aad74bff9c9d6
Wheng <Wheng@Wheng-VAIO>
2016-11-14 00:13:58 +0900

    fuck

--- src/main/java/spring/mvc/ROOT/repository/productRepository.java
+++ src/main/java/spring/mvc/ROOT/repository/productRepository.java
@@ -15,35 +15,35 @@ public class productRepository {
 	private SqlSessionTemplate sqlsesssiontemplate;
 	
 	public int insertOne(ProductDTO dto) {
-		return sqlsesssiontemplate.update("productMapper.insertOne",dto);
+		return sqlsesssiontemplate.update("Mapper.insertOne",dto);
 	}
 
 	public List<ProductDTO> selectVege() {
-		return sqlsesssiontemplate.selectList("productMapper.selectVege");
+		return sqlsesssiontemplate.selectList("Mapper.selectVege");
 	}
 
 	public List<ProductDTO> selectProcessed() {
-	return sqlsesssiontemplate.selectList("productMapper.selectProcessed");
+	return sqlsesssiontemplate.selectList("Mapper.selectProcessed");
 	}
 
 	public List<ProductDTO> selectMarine() {
-		return sqlsesssiontemplate.selectList("productMapper.selectMarine");
+		return sqlsesssiontemplate.selectList("Mapper.selectMarine");
 	}
 
 	public List<ProductDTO> selectMeat() {
-		return sqlsesssiontemplate.selectList("productMapper.selectMeat");
+		return sqlsesssiontemplate.selectList("Mapper.selectMeat");
 	}
 
 	public List<ProductDTO> selectGrain() {
-		return sqlsesssiontemplate.selectList("productMapper.selectGrain");
+		return sqlsesssiontemplate.selectList("Mapper.selectGrain");
 	}
 
 	public List<ProductDTO> selectFruit() {
-		return sqlsesssiontemplate.selectList("productMapper.selectFruit");
+		return sqlsesssiontemplate.selectList("Mapper.selectFruit");
 	}
 
 	public ProductDTO selectOne(Integer pronum) {
-		return sqlsesssiontemplate.selectOne("productMapper.selectOne",pronum);
+		return sqlsesssiontemplate.selectOne("Mapper.selectOne",pronum);
 	}
 
 }
